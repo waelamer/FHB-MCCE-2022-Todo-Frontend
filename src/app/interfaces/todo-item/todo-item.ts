77ca387ad085506fc4e682235bddf678dcf7a708
Kevin Horvath <kevin.horvath@gmx.at>
2022-03-26 20:38:53 +0100

    finished frontend & added connection to backend services

--- src/app/interfaces/todo-item/todo-item.ts
+++ src/app/interfaces/todo-item/todo-item.ts
@@ -1,12 +1,12 @@
 import { TodoItemOwner } from "./todo-item-owner";
 import { TodoItemState } from "./todo-item-state";
 
-export interface TodoItemModel {
+export interface TodoItem {
     id: string;
     name: string;
     description: string;
     state: TodoItemState;
-    owner: TodoItemOwner;
-    created: Date;
-    finished: Date;
+    owner?: TodoItemOwner;
+    created: string;
+    finished?: Date;
 }
